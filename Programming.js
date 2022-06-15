623d82fafc7bf64808482867b6780f4aeb607b34
hassanshabbir279 <87772719+hassanshabbir279@users.noreply.github.com>
2022-06-14 22:51:06 -0700

    Add files via upload

---
+++ Programming.js
@@ -0,0 +1,31 @@
+// Logical Oper
+
+//1. && 
+//2. ||
+
+
+// var marks = +prompt("Enter Your Marks?");
+// var grade;
+
+// if (marks >= 80 && marks <= 100) { 
+//   grade = "A+"
+// }else if (marks >= 70 && marks < 80){
+//     grade = "A"
+// }else if(marks >= 60 && marks < 80){
+//     grade = "B"
+// }else{
+//     grade = "Fail"
+// }
+
+// console.log(grade)
+
+
+
+// Data types
+
+// 1. undefined
+// 2. null
+// 3. boolean  .true / .false
+
+
+console.log(null || undefined);
